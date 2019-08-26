f90f8f7864f8362ca7585b53758b65a03cbd05e9
IvanMonroy <ivan.monroy@hrinteractive.co>
2019-08-26 09:19:53 -0500

    First service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
-import { HttpModule } from '@angular/http';
+/*import { HttpModule } from '@angular/http'; */
 
 
 @NgModule({
@@ -20,7 +20,7 @@ import { HttpModule } from '@angular/http';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    HttpModule,
+  /*  HttpModule, */
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'vehicles/:id', component: ProductDetailsComponent },
